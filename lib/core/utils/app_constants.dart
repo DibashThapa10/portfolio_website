9bed8fd0b348dd20963b77399274b578948026b8
Dibash Thapa <dibashthapa447@gmail.com>
2026-03-12 17:26:54 +0545

    update cv and add new project

--- lib/core/utils/app_constants.dart
+++ lib/core/utils/app_constants.dart
@@ -121,5 +121,15 @@ abstract class AppConstants {
       googlePlay:
           'https://play.google.com/store/apps/details?id=com.groease.app',
     ),
+    Project(
+      name: 'Lifeordo',
+      imageUrl:
+          'https://play-lh.googleusercontent.com/FVpiVgkB0ui4gIAJVTCtI9RLaZoaZzr4ghpSzx20iWnnK7TlRg63rTRxuoPTU2Cipy7HdYWeqAyYxaHz-t-N6mo=w480-h960-rw',
+      description:
+          'A productivity and life management app that enables users to manage tasks, habits, reminders, bills, and group collaboration from a single platform.',
+      //   appleStore: 'https://apps.apple.com/us/app/iprofixer-client/id6478153087',
+      googlePlay:
+          'https://play.google.com/store/apps/details?id=com.app.lifeordo',
+    ),
   ];
 }
